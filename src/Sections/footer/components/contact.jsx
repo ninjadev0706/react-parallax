74c4ff6f494f42c425584ff337a760e5854cb606
PureAffection <ninjadeveloper0706@gmail.com>
2022-12-12 10:34:39 +0900

    footer_visible

--- src/Sections/footer/components/contact.jsx
+++ src/Sections/footer/components/contact.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className='flex mt-20'>
-            <p className='footer flex'>All Rights Reserved © 2022 BllA Inc. &nbsp; <span className="text-[#ff7f00] md:flex hidden">Full website is coming soon ✌️</span> </p>
+            <p className='contact'>All Rights Reserved © 2022 BllA Inc. &nbsp;</p>
+            <p className="contact text-[#ff7f00] md:block hidden">Full website is coming soon ✌️</p>
         </div>
     )
 }
--- src/Sections/footer/footer.jsx
+++ src/Sections/footer/footer.jsx
@@ -7,7 +7,7 @@ import './styles.css'
 
 const Footer = () => {
     return (
-        <div className='title pt-[120px] xl:px-[300px] lg:px-[200px] md:px-[100px] px-[50px] bg-[#00000022]'>
+        <div className='title pt-[120px] xl:px-[300px] lg:px-[200px] md:px-[100px] px-[50px] pb-[50px] bg-[#00000022]'>
             <List />
             <Contact />
         </div>
