ebc8ed70c2d655dcefafd6aeea1fb48caf361b61
ninjadev0706 <ninjadeveloper0706@gmail.com>
2022-12-14 07:17:18 +0900

    mobile hero video

--- src/Sections/hero/components/heroVideo.jsx
+++ src/Sections/hero/components/heroVideo.jsx
@@ -8,7 +8,7 @@ const HeroVideo = () => {
   }, []);
   return (
     <div className="w-[100%] justify-end">
-      <div className="">
+      <div className="md:block hidden">
         <video
           playsInline
           autoPlay
@@ -19,6 +19,21 @@ const HeroVideo = () => {
         >
           <source
             src="./items/hero/hero.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className="md:hidden block">
+        <video
+          playsInline
+          autoPlay
+          muted
+          loop
+          ref={vidRef}
+          className="xl:rounded-[80px] rounded-[40px] w-[1000px]"
+        >
+          <source
+            src="./items/hero/mobile_hero.mp4"
             type="video/mp4"
           />
         </video>
